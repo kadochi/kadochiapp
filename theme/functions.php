c1cdb8085287587dd3e761222fbd9a01f945fdd6
kadochi <info@kadochi.com>
2026-07-10 17:47:29 +0330

    feat(theme): add WordPress admin theme

---
+++ theme/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Kadochi Headless Admin Theme functions.
+ */
+
+add_action( 'after_setup_theme', function () {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script' ) );
+} );
---
+++ theme/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+	<h1><?php bloginfo( 'name' ); ?></h1>
+	<p>This WordPress theme supports editor and administration workflows. The public site is served by Next.js.</p>
+</main>
+
+<?php get_footer(); ?>
